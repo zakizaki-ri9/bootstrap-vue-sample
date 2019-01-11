50efa30cee42fcdb32306d6fcbd41fcb235edbd9
zakizaki-ri9 <kic.yuuki@gmail.com>
2019-01-11 09:45:53 +0900

    fix

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -53,7 +53,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: 'index.pug',
+      filename: 'index.html',
       template: 'index.pug',
       inject: true
     }),
